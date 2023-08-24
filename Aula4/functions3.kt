45cfa12b618d4981757877387eed8bd3e9d45d3c
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2023-08-24 10:11:21 -0300

    Update functions3.kt

--- Aula4/functions3.kt
+++ Aula4/functions3.kt
@@ -1,44 +1,53 @@
-var numero = 0 //Variável global (fora das funções)
+var numero = 0 //Variável global (fora de qualquer função)
 
 fun main() {
-    println(numero)
-    criaLinha(100)
-    somaDez()
-    println(numero)
-    criaLinha(50)
-    somaQuinze()
-    println(numero)
-    criaLinha(10)
-    baskara(1.0, 4.0, 2.0) //Parâmetros posicionais
-    baskara(c = 1.0, a = 4.0, b = 2.0) //Parâmetros nomeados
-    baskara(b = 6.0, c = 3.0) //Não enviando o parâmetro "a"
+   println(numero)
+   somaDez()
+   	desenhaLinha(numero)
+    println( desenhaLinha2(numero) )
+   println(numero)
+   somaQuinze()
+   	desenhaLinha(numero)
+    println( desenhaLinha2(numero) )
+   println(numero)
+   
+   bhaskara(1.0, 4.0, 2.0) //Usando parâmetros POSICIONAIS
+   bhaskara(b = 8.0, a = 2.0, c = 4.0) //Usando parâmetros NOMEADOS
+   bhaskara(c = 4.0, b = 6.0) 
 }
 
 fun somaDez() {
-    numero+=10 // O mesmo que "numero = numero + 10"
+    numero+=10 //O mesmo que "numero = numero + 10"
 }
 
 fun somaQuinze() {
     numero+=15
 }
 
-fun criaLinha(quant : Int) {
-    var x = ""
-    for(i in 0..quant) {
-        x+="-"
+fun desenhaLinha(numero : Int) {
+    var linha = ""
+    for(i in 1..numero) {
+        linha+="-"
     }
-    println(x)
+    println(linha)
 }
 
-fun baskara(
-    a : Double = 1.0, //Valor padrão (caso não seja enviado)
+fun desenhaLinha2(numero : Int) : String {
+    var linha = ""
+    for(i in 1..numero) {
+        linha+="="
+    }
+    return linha
+}
+
+fun bhaskara(
+    a : Double = 1.0, //Valor padrão (opcional)
     b : Double,
     c : Double
 ) {
-    val delta = Math.pow(b,2.0) - (4.0 * a * c)    
-    val xLinha = (- b + Math.sqrt(delta)) / (2.0 * a)
-    val xDuasLinha = (- b - Math.sqrt(delta)) / (2.0 * a)
-    println("Delta: $delta")
-    println("X' = $xLinha")
-    println("X'' = $xDuasLinha")
+   val delta = (b * b) - (4 * a * c)
+   val x1 = (- b - Math.sqrt(delta) ) / 2 * a
+   val x2 = (- b + Math.sqrt(delta) ) / 2 * a
+   println("X' = $x1")
+   println("X'' = $x2")
 }
