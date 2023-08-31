21fb40f3ad59650247a508ccc85ed123e7c0b97f
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2023-08-31 12:21:26 -0300

    Update filter.kt

--- Aula3/filter.kt
+++ Aula3/filter.kt
@@ -1,38 +1,48 @@
-//Criando lista de dados (global)
+/// Código enviado por S4uszDev <Danilo Leipnitz>
+
+// Criando lista de dados (global)
 var listaCarros = mutableListOf(
-    	"Chevette",
-        "Gurgel",
-        "Chevi 500",
-        "Opala",
-        "Monza",
-        "Fusca",
-        "Kombi"
-    )
+    "Chevette",
+    "Gurgel",
+    "Chevi 500",
+    "Opala",
+    "Monza",
+    "Fusca",
+    "Kombi"
+)
 
 fun main() {
+
+    // Criando lista mutável para filtrar dados da outra lista
+    var listaFiltro: MutableList<String> = mutableListOf()
+
+    // Mostrando dados sem filtro
+    println("***********LISTA COMPLETA************")
+    listaCarros.forEach { println(it) }
     
-    //Criando lista mutável para filtrar dados da outra lista
-    var listaFiltro : MutableList<String> = mutableListOf()
-    
-    listaCarros.forEach{
-        if(it.toLowerCase().contains("ch")) {
+    //Filtrando
+    listaCarros.forEach {
+        if (it.toLowerCase().contains("ch")) {
             listaFiltro.add(it)
         }
     }
+
+    // Mostrando dados da lista de filtro
+    println("***********ELEMENTOS FILTRADOS***********")
+    listaFiltro.forEach { println(it) }
     
-    //Mostrando dados da lista de filtro
-    listaFiltro.forEach{ println(it) }
-    
-    apagaElemento("")
+    // Chama apagaElemento após a listagem
+    apagaElemento("ch") // Passando o filtro correto
+
+    // Lista após a remoção
+    println("*********LISTA APÓS EXCLUSÃO**********")
+    listaCarros.forEach { println(it) }
+    println("*************************************")
 }
 
-
-fun apagaElemento(filtro : String) {
-    listaCarros.forEach{
-        if(it.toLowerCase().contains(filtro)) {
-            listaCarros.remove(it)
-            println("Elemento excluído...")
-        }
+fun apagaElemento(filtro: String) {
+    listaCarros.removeIf { 
+        it.toLowerCase().contains(filtro)
     }
-    listaCarros.forEach{ println(it) }
+    println("Elementos excluídos!")
 }
