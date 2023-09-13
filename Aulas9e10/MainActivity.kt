d82167c9af88e632cb45cc79ede1770bed8fb2c6
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2023-09-12 21:50:30 -0300

    Update MainActivity.kt

--- Aulas9e10/MainActivity.kt
+++ Aulas9e10/MainActivity.kt
@@ -1,19 +1,30 @@
-package com.qi.aula9manha
+package com.qi.aula9noite
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Add
-import androidx.compose.material.icons.rounded.Lock
-import androidx.compose.material.icons.rounded.Person
-import androidx.compose.material3.*
+import androidx.compose.material.icons.outlined.AccountBox
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.CheckCircle
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,14 +39,15 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import com.qi.aula9manha.ui.theme.Aula9ManhaTheme
+import androidx.compose.ui.unit.dp
+import com.qi.aula9noite.ui.theme.Aula9NoiteTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Aula9ManhaTheme {
-                MyApp()
+            Aula9NoiteTheme {
+
             }
         }
     }
@@ -50,98 +62,119 @@ fun MyApp(
     }
 
     when(screen) {
-        0 -> Greeting(screen = {screen = 1})
+        0 -> Greeting(login = { screen = 1 })
         1 -> HomePage()
     }
 }
+
 
 @Composable
 fun HomePage(
     modifier: Modifier = Modifier
 ) {
-    Column() {
-        Text(text = "Home Page")
-    }
+    Text(text = stringResource(id = R.string.home_page))
 }
+
 
 @Composable
 fun Greeting(
-    screen: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    login: () -> Unit
 ) {
     Column(
+        modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceEvenly,
-        modifier = modifier.fillMaxSize()
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(text = stringResource(id = R.string.text_screen1))
-
-        MyTextField(
-            isPassword = false,
-            imageVector = Icons.Rounded.Person,
-            text = R.string.tf2,
-            keyboardType = KeyboardType.Text
+        Text(
+            text = stringResource(id = R.string.welcome),
+            style = MaterialTheme.typography.titleLarge
         )
+
+        MyTextField(icon = Icons.Outlined.AccountCircle)
 
         MyTextField(
             isPassword = true,
-            imageVector = Icons.Rounded.Lock,
-            text = R.string.tf2,
-            keyboardType = KeyboardType.Password
+            icon = Icons.Outlined.Lock,
+            keyboard = KeyboardType.Password
+        )
+
+        Divider(
+            thickness = 2.dp,
+            modifier = modifier.padding(40.dp)
         )
 
         Button(
-            onClick = screen
+            onClick = login
         ) {
-            Text(text = stringResource(id = R.string.btn1))
+            Text(
+                text = stringResource(id = R.string.btn_login),
+                style = MaterialTheme.typography.titleMedium
+            )
         }
     }
 }
 
-////////////////////////////Criando compomente "TextField" reutilizável
+
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyTextField(
-    isPassword: Boolean,
-    imageVector: ImageVector,
-    @StringRes text: Int,
-    keyboardType: KeyboardType,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    isPassword: Boolean = false,
+    icon: ImageVector,
+    keyboard: KeyboardType = KeyboardType.Text
 ) {
+    var text by remember {
+        mutableStateOf(TextFieldValue(""))
+    }
+
     var showPass by remember {
         mutableStateOf(false)
     }
 
-    var textField by remember {
-        mutableStateOf(TextFieldValue(""))
-    }
-
-    TextField(
-        value = textField,
-        onValueChange = { textField = it },
-        leadingIcon = {
-            Icon(
-                imageVector = imageVector,
-                contentDescription = null,
-                modifier = if(isPassword) modifier.clickable { showPass = !showPass } else modifier
+    Surface(
+        color = MaterialTheme.colorScheme.onTertiary,
+        shape = MaterialTheme.shapes.large,
+        modifier = modifier.padding(10.dp)
+    ) {
+        Row {
+            TextField(
+                value = text,
+                onValueChange = { text = it },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = keyboard
+                ),
+                leadingIcon = {
+                    Icon(
+                        imageVector = icon,
+                        contentDescription = null
+                    )
+                },
+                trailingIcon = {
+                    if(isPassword) {
+                        Icon(
+                            imageVector =
+                            if (showPass) Icons.Outlined.Favorite
+                            else Icons.Outlined.FavoriteBorder,
+                            contentDescription = null,
+                            modifier = modifier.clickable { showPass = !showPass }
+                        )
+                    }
+                },
+                visualTransformation =
+                    if(!isPassword || showPass) VisualTransformation.None
+                    else PasswordVisualTransformation('?')
             )
-        },
-        placeholder = {
-            Text(text = stringResource(id = text))
-        },
-        visualTransformation = if (showPass || !isPassword) VisualTransformation.None else PasswordVisualTransformation('X'),
-        keyboardOptions = KeyboardOptions(
-            keyboardType = keyboardType
-        ),
-    )
+        }
+    }
 }
-///////////////////////////////////////////////////////////////////////////
+
 
 @Preview(showBackground = true, widthDp = 320, heightDp = 480)
 @Composable
-fun DefaultPreview() {
-    Aula9ManhaTheme {
-        //Greeting()
-        //HomePage()
+fun GreetingPreview() {
+    Aula9NoiteTheme {
         MyApp()
     }
 }
